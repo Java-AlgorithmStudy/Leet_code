f1907a33fa5abaa450f774f63ceafe41b607babd
sunminnnnn <102842850+sunminnnnn@users.noreply.github.com>
2023-07-26 23:17:24 +0900

    Update S_392. Is Subsequence.java

--- 3주차/S_392. Is Subsequence.java
+++ 3주차/S_392. Is Subsequence.java
@@ -1,8 +1,27 @@
 class Solution {
-    public boolean isSubsequence(String s, String t) {
+    public boolean isSubsequence1(String s, String t) {
         int sIndex = 0;
         for (int tIndex=0; sIndex<s.length() && tIndex<t.length(); tIndex++)
             if (s.charAt(sIndex) == t.charAt(tIndex)) sIndex++;
         return sIndex == s.length();
     }
+    // runtime이 2ms / memory가 40.31mb
+
+    public boolean isSubsequence2(String s, String t) {
+        int sLeft = 0, tLeft = 0;
+        int sRight = s.length(), tRight = t.length();
+        char[] tArray = t.toCharArray();
+        char[] sArray = s.toCharArray();
+
+        if (sRight < 1) return true;
+
+        while (tRight > tLeft) {
+            if (tArray[tLeft] == sArray[sLeft]) sLeft++;
+            tLeft++;
+            if (sLeft == sRight) return true;
+        }
+        
+        return false;
+    }
+    // runtime이 0ms / memory가 40.6mb
 }
